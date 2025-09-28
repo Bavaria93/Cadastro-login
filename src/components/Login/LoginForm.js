60e90c761df0f7314977a9d9083496779151fef3
Raphael Câmara <33887019+Bavaria93@users.noreply.github.com>
2025-09-28 19:06:04 -0300

    ajustes na organização de "Login" e "LoginForm"

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -3,7 +3,6 @@ import { InputAdornment } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 import useLoginForm from '../../hooks/useLoginForm';
 import {
-  LoginContainer,
   FormBox,
   Title,
   StyledTextField,
@@ -20,48 +19,46 @@ export default function LoginForm() {
   } = useLoginForm();
 
   return (
-    <LoginContainer maxWidth="sm">
-      <FormBox> {/* nenhum form nativo */}
-        <Title variant="h4">Login</Title>
+    <FormBox>
+      <Title variant="h4">Login</Title>
 
-        <StyledTextField
-          label="Email"
-          type="email"
-          autoComplete="username"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
+      <StyledTextField
+        label="Email"
+        type="email"
+        autoComplete="username"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
 
-        <StyledTextField
-          label="Senha"
-          type={showPassword ? 'text' : 'password'}
-          autoComplete="current-password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <PasswordAdornmentButton
-                  onClick={() => setShowPassword(prev => !prev)}
-                  edge="end"
-                  aria-label="toggle password visibility"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </PasswordAdornmentButton>
-              </InputAdornment>
-            )
-          }}
-        />
+      <StyledTextField
+        label="Senha"
+        type={showPassword ? 'text' : 'password'}
+        autoComplete="current-password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <PasswordAdornmentButton
+                onClick={() => setShowPassword(prev => !prev)}
+                edge="end"
+                aria-label="toggle password visibility"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </PasswordAdornmentButton>
+            </InputAdornment>
+          )
+        }}
+      />
 
-        <SubmitButton
-          type="button"
-          onClick={handleSubmit}
-          variant="contained"
-          fullWidth
-        >
-          Entrar
-        </SubmitButton>
-      </FormBox>
-    </LoginContainer>
+      <SubmitButton
+        type="button"
+        onClick={handleSubmit}
+        variant="contained"
+        fullWidth
+      >
+        Entrar
+      </SubmitButton>
+    </FormBox>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { LoginContainer } from '../components/Login/Login.styles';
 import LoginForm from '../components/Login/LoginForm';
 
 export default function Login() {
-  return <LoginForm />;
+  return (
+    <LoginContainer maxWidth="sm">
+      <LoginForm />
+    </LoginContainer>
+  );
 }
